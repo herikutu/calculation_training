bbab702f28d543fa8285d4872d43f0e3de33104c
suguru.chinen <suguru.chinen@gmail.com>
2026-06-30 15:13:26 +0900

    Configure Firebase project

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 window.CALC_TRAINING_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyCeIUssUKuGx6Qs_XcBqan0jaQxhEc6fkA",
+  authDomain: "calculation-training-37725.firebaseapp.com",
+  projectId: "calculation-training-37725",
+  storageBucket: "calculation-training-37725.firebasestorage.app",
+  messagingSenderId: "300869430183",
+  appId: "1:300869430183:web:5669b19eac024eeef2e482",
 };
